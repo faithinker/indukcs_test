29004fe35435b22322fd8efd3b6b07abe9234843
faithinker <faithinker12@gmail.com>
2022-04-11 12:26:12 +0900

    wek05-React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+import MyComponentApp from './Do-it-React/MyComponentApp';
+
+class App extends React.Component {
+  render() {
+    return (
+      <MyComponentApp/>
+    );
+  }
+}
+export default App;
